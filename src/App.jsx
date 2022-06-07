b520022368f6568e3283aad721a81f8f858fd5a2
Gavin Stirling <gavinstir@gmail.com>
2022-06-07 17:19:16 +0100

    Set EmployeeTile up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.scss';
 
 import team from './data/employees';
 
+import EmployeeTile from "./components/EmployeeTile/EmployeeTile";
+
 
 console.log(team);
 
@@ -9,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Gavin's Ticket Tracker</h1>
+      <EmployeeTile />
     </div>
   );
 }
