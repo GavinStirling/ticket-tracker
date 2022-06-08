6224d34944d3c1d6c4f8b97e13f919fe6db1a22d
Gavin Stirling <gavinstir@gmail.com>
2022-06-08 15:27:47 +0100

    Completed functional task

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
+// App styling
 import './App.scss';
 
+// Importing data to be used
 import team from './data/employees';
 
+// Importing components
 import Grid from './components/Grid/Grid';
-import EmployeeTile from "./components/EmployeeTile/EmployeeTile"
-
-console.log(team);
 
 function App() {
   return (
     <div className="App">
-      <h1>Gavin's Ticket Tracker</h1>
-      <Grid />
-      <EmployeeTile />
+      <Grid className="grid__employees" arr={team} />
     </div>
   );
 }
